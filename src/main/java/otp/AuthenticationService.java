98f7fe3ff8600b0e1912f837b9424a7a618c8b21
Joey Chen <jay.hatelove@gmail.com>
2023-01-06 20:00:22 +0800

    use Mockito with annotation for stub dependency behavior/value

--- src/main/java/otp/AuthenticationService.java
+++ src/main/java/otp/AuthenticationService.java
@@ -1,16 +1,25 @@
 package otp;
 
 public class AuthenticationService {
+
+    private final IProfile profile;
+    private final IToken token;
+
+    public AuthenticationService(IProfile profile, IToken token) {
+        this.profile = profile;
+        this.token = token;
+    }
+
+    public AuthenticationService() {
+        profile = new ProfileDao();
+        token = new RsaTokenDao();
+    }
+
     public boolean isValid(String account, String password) {
-        // 根據 account 取得自訂密碼
-        ProfileDao profileDao = new ProfileDao();
-        String passwordFromDao = profileDao.getPassword(account);
+        String passwordFromDao = profile.getPassword(account);
 
-        // 根據 account 取得 RSA token 目前的亂數
-        RsaTokenDao rsaToken = new RsaTokenDao();
-        String randomCode = rsaToken.getRandom(account);
+        String randomCode = token.getRandom(account);
 
-        // 驗證傳入的 password 是否等於自訂密碼 + RSA token亂數
         String validPassword = passwordFromDao + randomCode;
         boolean isValid = password.equals(validPassword);
 
--- src/test/java/otp/AuthenticationServiceTest.java
+++ src/test/java/otp/AuthenticationServiceTest.java
@@ -1,17 +1,41 @@
 package otp;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class AuthenticationServiceTest {
+    @Mock
+    private IProfile profile;
+    @Mock
+    private IToken token;
+    @InjectMocks
+    private AuthenticationService authenticationService;
 
-    @Test
-    public void is_valid_test() {
-        AuthenticationService target = new AuthenticationService();
+    @Nested
+    class WhenCheckingIfIsValid {
 
-        boolean actual = target.isValid("joey", "91000000");
+        @Test
+        public void valid() {
+            when(profile.getPassword("joey")).thenReturn("91");
+            when(token.getRandom(anyString())).thenReturn("000000");
 
-        assertTrue(actual);
+            boolean actual = authenticationService.isValid("joey", "91000000");
+            assertTrue(actual);
+        }
+
+        @BeforeEach
+        void setup() {
+        }
     }
 }
